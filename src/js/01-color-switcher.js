9017b0724ac63f556adfe32e7309234eebb6f8d3
Yanat58 <tanyalyt80@gmail.com>
2023-03-07 13:41:30 +0200

    task-01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stoptBtnRef = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// const color = getRandomHexColor();
+// document.body.style.backgroundColor = color;
+startBtnRef.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+   timerId = setInterval(()=> {
+    document.body.style.backgroundColor = getRandomHexColor()
+   }, 1000) 
+   startBtnRef.disabled = true ;
+   stoptBtnRef.disabled = false; 
+}
+
+stoptBtnRef.addEventListener('click', ()=> {
+   clearInterval(timerId);
+   startBtnRef.disabled = false ;
+   stoptBtnRef.disabled = true; 
+})
 
